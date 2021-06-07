9b10ec9fa13f3f78f15e60e36de5b4520475ffb6
Heshani Wickramarachchi <67557155+Heshani99@users.noreply.github.com>
2021-06-07 07:33:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className ="note" >
+      <h1>JavaScript and React.js</h1>
+        <p> This was an amazing bootcamp. 
+            I'm a university student from Sri Lanka and this bootcamp helped me alot to catch up with my studies.
+            Thank you so much for conducting such an amazing bootcamp and would like join in the future bootcamp series as well.
+        </p>
+    </div>
+  );
+}
+
+export default Note;
+
